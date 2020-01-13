203cb74d358d36068119fa324149ac6274b92922
BonitaG <bonitaguetta@gmail.com>
2020-01-13 22:49:50 +0600

    hw5.3

--- src/com/company/Magic.java
+++ src/com/company/Magic.java
@@ -11,24 +11,13 @@ public class Magic {
         this.damage = damage;
         this.superAbility = superAbility;
     }
-
-    public Hero[] creation() {
-        Hero[] heroes = new Hero[4];
-        Hero warrior = new Hero(150, 200, " Physical ");
-        heroes[0] = warrior;
-        Hero magic = new Hero(150, 200, " Magic ");
-        heroes[1]= magic;
-        Hero mental =new Hero (150, 200, " Mental ");
-        heroes[2]=mental;
-        Hero heal =new Hero (150, 200, " Heal ");
-        heroes[3]=heal;
-        return heroes;
-
-
-
-
-
-
-
-    }
 }
+
+
+
+
+
+
+
+
+
